1d783a4e9b93692f82a929e8fc25febb69b9829f
Mihnea <m@mihneaparvanu.ro>
2026-04-19 21:59:49 +0300

    chore: remove scripts directory

--- scripts/list-models.ts
+++
@@ -1,9 +0,0 @@
-import postgres from 'postgres';
-import { drizzle } from 'drizzle-orm/postgres-js';
-import { houseModels } from '../src/db/schema.ts';
-
-const client = postgres('postgresql://boholz:zuDja2-ciqfys-dikcap@192.168.1.180:5433/boholz-db', { ssl: false });
-const db = drizzle(client);
-const rows = await db.select({ id: houseModels.id, code: houseModels.modelCode, title: houseModels.title }).from(houseModels);
-rows.forEach(r => console.log(`${r.code}  ${r.title}`));
-await client.end();
